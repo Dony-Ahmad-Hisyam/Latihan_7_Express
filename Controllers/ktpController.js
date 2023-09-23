ae838de4d1a0b3ab06b29a2378f0ba8b02cd231c
Dony-Ahmad-hisyam <donyahmad4321@gmail.com>
2023-09-23 07:14:14 +0700

    CRUD KTP DONE

--- Controllers/ktpController.js
+++ Controllers/ktpController.js
@@ -1,7 +1,25 @@
 const connection = require("../Models/Config/db");
 const { body, validationResult } = require("express-validator");
 
+const getAllKtpController = (req, res) => {
+  connection.query("SELECT * FROM ktp", (err, rows) => {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: "Server Error",
+      });
+    } else {
+      return res.status(200).json({
+        status: true,
+        message: "Sukses..!",
+        data: rows,
+      });
+    }
+  });
+};
+
 const addKtpController = [
+  body("nik").notEmpty(),
   body("nama_lengkap").notEmpty(),
   body("jenis_kelamin").notEmpty().isIn(["Laki-laki", "Perempuan"]),
   body("tempat_lahir").notEmpty(),
@@ -21,6 +39,7 @@ const addKtpController = [
     }
 
     const data = {
+      nik: req.body.nik,
       nama_lengkap: req.body.nama_lengkap,
       jenis_kelamin: req.body.jenis_kelamin,
       tempat_lahir: req.body.tempat_lahir,
@@ -48,5 +67,103 @@ const addKtpController = [
     });
   },
 ];
+const getByIdKtpController = (req, res) => {
+  const ktpNik = req.params.nik; // Ambil nik dari parameter URL
+  connection.query("SELECT * FROM ktp WHERE nik = ?", [ktpNik], (err, rows) => {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: "Server Error",
+      });
+    } else {
+      if (rows.length === 0) {
+        return res.status(404).json({
+          status: false,
+          message: "Data not found",
+        });
+      } else {
+        return res.status(200).json({
+          status: true,
+          message: "Sukses..!",
+          data: rows[0],
+        });
+      }
+    }
+  });
+};
 
-module.exports = { addKtpController };
+const editKtpController = [
+  body("nik").notEmpty(),
+  body("nama_lengkap").notEmpty(),
+  body("jenis_kelamin").notEmpty().isIn(["Laki-laki", "Perempuan"]),
+  body("tempat_lahir").notEmpty(),
+  body("tanggal_lahir").notEmpty(),
+  body("agama").notEmpty().isIn(["Islam", "Kristen"]),
+  body("pendidikan").notEmpty(),
+  body("jenis_pekerjaan").notEmpty(),
+  body("golongan_darah").notEmpty(),
+  body("kewarganegaraan").notEmpty(),
+
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array(),
+      });
+    }
+
+    const ktpNik = req.params.nik;
+    const data = {
+      nik: req.body.nik,
+      nama_lengkap: req.body.nama_lengkap,
+      jenis_kelamin: req.body.jenis_kelamin,
+      tempat_lahir: req.body.tempat_lahir,
+      tanggal_lahir: req.body.tanggal_lahir,
+      agama: req.body.agama,
+      pendidikan: req.body.pendidikan,
+      jenis_pekerjaan: req.body.jenis_pekerjaan,
+      golongan_darah: req.body.golongan_darah,
+      kewarganegaraan: req.body.kewarganegaraan,
+    };
+
+    connection.query("UPDATE ktp SET ? WHERE nik = ?", [data, ktpNik], (err, rows) => {
+      if (err) {
+        return res.status(500).json({
+          status: false,
+          message: "Server Error",
+        });
+      } else {
+        return res.status(200).json({
+          status: true,
+          message: "Sukses..!",
+          data: rows[0],
+        });
+      }
+    });
+  },
+];
+
+const deleteKtpController = (req, res) => {
+  const ktpNik = req.params.nik; //
+  connection.query("DELETE FROM ktp WHERE nik = ?", [ktpNik], (err, rows) => {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: "Server Error",
+      });
+    } else {
+      return res.status(200).json({
+        status: true,
+        message: "Data deleted successfully",
+      });
+    }
+  });
+};
+
+module.exports = {
+  getAllKtpController,
+  getByIdKtpController,
+  addKtpController,
+  editKtpController,
+  deleteKtpController,
+};
--- Views/ktpRoute.js
+++ Views/ktpRoute.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { addKtpController } = require("../Controllers/ktpController");
+const { getAllKtpController, getByIdKtpController, addKtpController, editKtpController, deleteKtpController } = require("../Controllers/ktpController.js");
 
-router.post("/api/add-ktp", addKtpController);
+router.get("/api/ktp", getAllKtpController);
+router.get("/api/ktp/:nik", getByIdKtpController);
+router.post("/api/ktp/add", addKtpController);
+router.put("/api/ktp/update/:nik", editKtpController);
+router.delete("/api/ktp/delete/:nik", deleteKtpController);
 
 module.exports = router;
